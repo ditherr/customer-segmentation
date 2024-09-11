dc0ceea626a3261a53ce9b2d16dee6535f2552b1
ditherr <adityahh.rm@gmail.com>
2024-09-11 20:52:54 +0700

    v1 - application build

--- app.py
+++ app.py
@@ -1,1 +1,132 @@
 # Build App to Predict Cluster and Give the Information for each Clusters
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+import time
+from src.pipeline.km_train import CustomData
+from src.pipeline.km_train import kmeans_model, find_best_k, predict_pipe
+import plotly.express as px
+import plotly.graph_objs as go
+from src.analysis import cluster2d, cluster3d, load_model, convert_df, upload_file, show_gender, clustered
+
+## Dataset
+dataset = pd.read_csv('data/Mall_Customers.csv')
+sample = pd.read_csv('data/Mall_Customers.csv')
+selected_feature = ["Annual Income (k$)", "Spending Score (1-100)"]
+selected_features = ["Age", "Annual Income (k$)", "Spending Score (1-100)"]
+X = dataset[selected_feature]
+X2 = dataset[selected_features]
+
+
+def main():
+    st.set_page_config(
+        page_title="Customer Segmentation",
+        page_icon="🏬",
+        layout="centered"
+    )
+    
+    st.title("Customer Segmentation")
+    st.markdown("""This dataset contains Customer behavior based on their annual income and expenditure scores in a Mall""")
+    
+    st.subheader('📋Existing Dataset')
+    dataset.columns = ['Customer ID', 'Gender', 'Age', 'Annual Income (k$)', 'Spending Score (1-100)']
+    with st.expander("👀 Show Datasets"):
+        st.page_link("https://www.kaggle.com/datasets/vjchoudhary7/customer-segmentation-tutorial-in-python", label="Kaggle - Mall Customer Segmentation", icon="🗃️")
+        st.write(dataset)
+    
+    st.subheader('🕹️Play with Clustering')
+    st.markdown("""Play with clustering, how the cluster can affect the data. :blue-background[**Let's try and see**].\
+        Show in two style, Cluster 2D and Cluster 3D, you can choose both.""")
+    with st.expander("🗺️ Do clustering"):
+        cl, ch = st.columns(2)
+        with cl:
+            cluster = st.slider("Choose Cluster", 1, 10)
+        with ch:
+            st.write("**Choose the Cluster style**")
+            cl2d = st.checkbox("Cluster 2D")
+            cl3d = st.checkbox("Cluster 3D")
+            
+            
+        if cl2d:
+            data, model = load_model(cluster, 1, X)
+            cluster2d(data, model)
+        if cl3d:
+            data, model = load_model(cluster, 2, X2)
+            cluster3d(data, model)
+    
+    st.subheader('🕵️Try to Predict')
+    st.markdown("""The prediction use the dataset above, you can use your dataset but you need to :blue-background[**have same features or columns like ours**].\
+        You can do the prediction by input a single data or using our sample here:""")
+
+
+    csv = convert_df(sample)
+
+    st.download_button(
+        label="Download sample",
+        data=csv,
+        file_name="sample.csv",
+        mime="text/csv",
+    )
+    with st.expander("🕵️ Predict please...."):
+        st.info("ℹ️ - **prediction will use our model (*KMeans with K=5*)**")
+        pred = st.radio("Pick your options...", options=["Single", "Datasets"])
+        
+        if pred == 'Single':
+            with st.form('Predict Cluster', clear_on_submit=True):
+                p1, p2 = st.columns(2)
+                with p1:
+                    gender = st.selectbox("Gender",['Male', 'Female'], index=None, placeholder='Gender...')
+                    age = st.number_input("Age", min_value=17, max_value=70, step=1, placeholder="Age....")
+                with p2:
+                    income = st.number_input("Anual Income (k$)", min_value=10, max_value=150, step=1, placeholder="Annual Income....")
+                    spending = st.number_input("Spending Score (1-100)", min_value=1, max_value=100, step=1, placeholder="Your Age....")
+                
+                submitted = st.form_submit_button('Predict')
+                
+                if submitted:
+                    if not gender or not age or not income or not spending: 
+                        st.warning('Please completed the input field !!')
+                    else:
+                        data = CustomData(gender, age, income, spending)
+                        df = data.to_df()
+                        
+                        # predict the result
+                        result = predict_pipe(df)
+                        st.success(f'Based on your input, You will clustered into **Cluster {result}**', icon="🔎")
+            
+
+        else:
+            uploaded_file = st.file_uploader("Choose a CSV file")
+            
+            if uploaded_file is not None:
+                data_sample = pd.read_csv(uploaded_file)
+                result = predict_pipe(data_sample)
+                data_sample['Cluster'] = result
+                
+                
+                progress_text = "Your Data is being Clustered and Analyzed. Please wait."
+                my_bar = st.progress(0, text=progress_text)
+                for percent_complete in range(100):
+                    time.sleep(0.01)
+                    my_bar.progress(percent_complete + 1, text=progress_text)
+                time.sleep(1)
+                my_bar.empty()
+                
+                # Show success message
+                st.success("🎉 **Congrats! Your data has been successfully clustered**")
+                st.write(data_sample)
+                
+                
+                show_gender(data_sample)
+                clustered(data_sample)
+            else:
+                st.warning('Please upload the datasets')
+                
+            
+
+            
+            
+    
+if __name__ == '__main__':
+    main()
--- src/analysis.py
+++ src/analysis.py
@@ -1,1 +1,146 @@
 # Helper function to analysis
+import os
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from src.pipeline.km_train import kmeans_model, find_best_k
+from src.utils import load_object
+
+dataset = pd.read_csv('data/Mall_Customers.csv')
+coloring = ["red", "green", "blue", "purple", "orange", "magenta", "cyan", "yellow", "brown", "indigo"]
+
+kmeans_m = os.path.join('data', 'model_kmeans.pkl')
+pred_model = load_object(kmeans_m)
+
+## Transform as a string for coloring purpose
+def transform_cluster(data):
+    data = data.sort_values(by='Cluster', ascending=True)
+    data['Cluster'] = data['Cluster'].apply(lambda x: x+1)
+    data['Cluster'] = data['Cluster'].astype(str)
+    
+    return data
+
+
+## Load model
+def load_model(cluster, model, data):
+    if model == 1:
+        model = kmeans_model(cluster, data)
+        data_clustered = dataset.copy()
+        data_clustered['Cluster'] = model.labels_
+        data = transform_cluster(data_clustered)
+        return data, model
+    elif model == 2:
+        model = kmeans_model(cluster, data)
+        data_clustered = dataset.copy()
+        data_clustered['Cluster'] = model.labels_
+        data = transform_cluster(data_clustered)
+        return data, model
+
+
+## Draw a 2D Cluster
+def cluster2d(data, model):  
+    fig = px.scatter(data,
+                 x="Annual Income (k$)",
+                 y="Spending Score (1-100)",
+                 color="Cluster",
+                 color_discrete_sequence=coloring)
+    
+    # Add cluster centers to the plot
+    fig.add_trace(go.Scatter(
+        x=model.cluster_centers_[:, 0],
+        y=model.cluster_centers_[:, 1],
+        mode='markers',
+        marker=dict(color='black', size=15, symbol='x'),
+        name='Cluster Centers'
+    ))
+
+    fig.update_traces(marker_size=10)
+    fig.update_layout(title='Clustering in 2D', plot_bgcolor='white', showlegend=True)
+        
+    st.plotly_chart(fig)
+    
+
+## Draw a 3D Cluster
+def cluster3d(data, model):
+    fig = px.scatter_3d(data,
+                x='Age',
+                y='Annual Income (k$)',
+                z='Spending Score (1-100)',
+                color="Cluster",
+                color_discrete_sequence=coloring,
+                size_max=10)
+    
+    # Add cluster centers to the 3D plot
+    fig.add_trace(go.Scatter3d(
+        x=model.cluster_centers_[:, 0],  # Age
+        y=model.cluster_centers_[:, 1],  # Annual Income
+        z=model.cluster_centers_[:, 2],  # Spending Score
+        mode='markers',
+        marker=dict(color='black', size=6, symbol='x'),
+        name='Cluster Centers'
+    ))
+    
+    fig.update_layout(title='Clustering in 3D', plot_bgcolor='white')
+
+    
+    st.plotly_chart(fig)
+    
+def clustered(data):
+    data = transform_cluster(data)
+
+    fig = px.scatter_3d(data,
+                x='Age',
+                y='Annual Income (k$)',
+                z='Spending Score (1-100)',
+                color="Cluster",
+                color_discrete_sequence=coloring,
+                size_max=10)
+    
+    fig.update_layout(title='Clustering in 3D', plot_bgcolor='white')
+
+    st.plotly_chart(fig)
+    
+    
+    
+# Downloading PDF
+def convert_df(df):
+    return df.to_csv(index=False).encode("utf-8")
+
+
+# Load a file
+def upload_file(file):
+    if file is not None:
+        dataframe = pd.read_csv(file)
+    
+        return dataframe
+    else:
+        st.warning('Please upload the datasets')
+        
+        
+# Kmeans label predict
+def show_gender(data):
+    color_list = ['#EB455F', '#BAD7E9']
+    count_gender = data['Gender'].value_counts()
+    df = pd.DataFrame({'Gender': count_gender.index, 'Total': count_gender.values})
+
+    fig = make_subplots(rows=1, cols=2, specs=[[{"type": "bar"}, {"type": "pie"}]])
+
+    bar_trace = go.Bar(x=df.Gender, y=df.Total, marker=dict(color=color_list), text=df.Total, textposition='auto')
+    pie_trace = go.Pie(labels=df.Gender, values=df.Total, marker=dict(colors=color_list), textinfo='percent+label')
+
+    fig.add_trace(bar_trace, row=1, col=1)
+    fig.add_trace(pie_trace, row=1, col=2)
+
+    # Update layout
+    fig.update_layout(
+        title_text="Gender Distribution in Dataset",  # Title for the entire figure
+        yaxis=dict(title="Count", gridcolor='lightgray'),
+        showlegend=False,  # Disable the shared legend
+        plot_bgcolor='white',  # Set plot background to white
+        width=720
+    )
+
+    # Display the figure
+    st.plotly_chart(fig)
